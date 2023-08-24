ed3154dbc2a9956ac582b9ec16ff0660f247629c
Paulo Abrantes <paulo.abrantes@qub-it.com>
2023-08-24 10:49:03 +0100

    Correctly finding units in MoreThanOneStrategy
    Imagine the following scenario:
    
        TopUnitOne             TopUnitTwo
            |                     |
          SomeSchool             SomeOtherSchool
            |                      |
            |                     Deparment
            |                        |
            -------------------------
                      |
                  DegreeUnit
    
    And that we say to the more than one strategy that TopUnitOne,
    SomeSchool and SomeOtherSchool are the 3 available units.
    
    If we start in DegreeUnit the expected result is:
    
    SomeSchool and SomeOtherSchool in case we allow multiple units
    and
    SomeSchool in case in don't
    
    Notice that even when using multiple units TopUnitOne is not
    expected since we found SomeSchool. This means that when finding a unit,
    we do not continue the search to it's parents.
    
    That constraint is that this patch is fixing, since before this the
    result would be
    
    TopUnitOne, SomeSchoo, SomeOtherSchool
    
    which is incorrect.

--- src/main/java/com/qubit/solution/fenixedu/integration/cgd/services/utils/MoreThanOneAssociatedUnitStrategy.java
+++ src/main/java/com/qubit/solution/fenixedu/integration/cgd/services/utils/MoreThanOneAssociatedUnitStrategy.java
@@ -1,8 +1,14 @@
 package com.qubit.solution.fenixedu.integration.cgd.services.utils;
 
+import static java.util.Optional.ofNullable;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.fenixedu.academic.domain.organizationalStructure.Unit;
 import org.fenixedu.academic.domain.student.Registration;
@@ -13,49 +19,41 @@ public class MoreThanOneAssociatedUnitStrategy extends BaseIESCodeProviderStrate
 
     @Override
     public Set<String> getSchoolCodes(Registration registration) {
-        Unit associatedUnit = registration.getDegree().getUnit();
+        CgdIntegrationConfiguration instance = CgdIntegrationConfiguration.getInstance();
 
-        if (associatedUnit == null) {
-            return super.getSchoolCodes(registration);
-        }
+        return ofNullable(registration.getDegree().getUnit())
+                .map(degreeUnit -> breadthFirstSearch(degreeUnit, instance.getUnitsSet(), instance.getAllowsMultipleUnits()))
+                .orElseGet(() -> super.getSchoolCodes(registration));
 
-        return CgdIntegrationConfiguration.getInstance().getAllowsMultipleUnits() ? findAllUnitsCodes(registration,
-                associatedUnit) : findFirstUnitFromConfiguration(registration, associatedUnit);
     }
 
-    private Set<String> findAllUnitsCodes(Registration registration, Unit associatedUnit) {
-        Set<Unit> allowedUnits = CgdIntegrationConfiguration.getInstance().getUnitsSet();
-        Set<String> result = new HashSet<String>();
+    private Set<String> breadthFirstSearch(Unit startUnit, Set<Unit> acceptedUnits, boolean findFirst) {
+        List<Unit> foundUnits = new ArrayList<>();
+        Set<Unit> visited = new HashSet<>();
+        Queue<Unit> queue = new LinkedList<>();
 
-        if (allowedUnits.contains(associatedUnit)) {
-            result.add(associatedUnit.getCode());
-        }
-        associatedUnit.getAllParentUnits().stream().filter(unit -> allowedUnits.contains(unit))
-                .forEach(unit -> result.add(unit.getCode()));
-
-        return result.isEmpty() ? super.getSchoolCodes(registration) : result;
-    }
-
-    private Set<String> findFirstUnitFromConfiguration(Registration registration, Unit associatedUnit) {
-        Set<Unit> allowedUnits = CgdIntegrationConfiguration.getInstance().getUnitsSet();
-        LinkedList<Unit> queue = new LinkedList<Unit>();
-        queue.add(associatedUnit);
+        queue.offer(startUnit);
+        visited.add(startUnit);
 
         while (!queue.isEmpty()) {
-            Unit unit = queue.poll();
+            Unit currentUnit = queue.poll();
 
-            if (allowedUnits.contains(unit)) {
-                //We found the first unit in BFS that is a school unit.
-                //We set the code variable with the unit code and break the cycle.
-                return Set.of(unit.getCode());
-            } else {
-                //Polled unit is not a school unit so we add unit parents to continue the search
-                queue.addAll(unit.getParentUnits());
+            if (acceptedUnits.contains(currentUnit)) {
+                foundUnits.add(currentUnit);
+                if (findFirst) {
+                    break;
+                }
             }
 
+            if (!foundUnits.contains(currentUnit)) {
+                currentUnit.getParentUnits().stream().filter(parent -> !visited.contains(parent)).forEach(parent -> {
+                    queue.offer(parent);
+                    visited.add(parent);
+                });
+            }
         }
-        //We didn't find any school unit
-        return super.getSchoolCodes(registration);
+
+        return foundUnits.stream().map(Unit::getCode).collect(Collectors.toSet());
     }
 
     @Override
