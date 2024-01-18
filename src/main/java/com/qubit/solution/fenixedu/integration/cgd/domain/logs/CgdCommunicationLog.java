4d835af0f2c2286dd0a5410809912f6d4922773e
Miguel Morais <miguel.morais@qub-it.com>
2024-01-18 23:55:05 +0000

    Adds dataSent field to CgdCommunicationLog

--- src/main/java/com/qubit/solution/fenixedu/integration/cgd/domain/logs/CgdCommunicationLog.java
+++ src/main/java/com/qubit/solution/fenixedu/integration/cgd/domain/logs/CgdCommunicationLog.java
@@ -1,10 +1,17 @@
 package com.qubit.solution.fenixedu.integration.cgd.domain.logs;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.bennu.core.domain.Bennu;
@@ -25,10 +32,11 @@ public class CgdCommunicationLog extends CgdCommunicationLog_Base {
 
     @Atomic
     public static CgdCommunicationLog createCgdCommunicationLog(Registration registration, boolean requestCard, boolean success,
-            Person sender, String message, String exceptionStackTrace) {
+            Person sender, String message, String exceptionStackTrace, String dataSent) {
         CgdCommunicationLog communicationLog = new CgdCommunicationLog(registration, requestCard, success, sender);
         communicationLog.setMessage(message);
         communicationLog.setExceptionStackTrace(exceptionStackTrace);
+        communicationLog.setDataSent(dataSent == null || dataSent.isBlank() ? "" : compressData(dataSent));
         return communicationLog;
     }
 
@@ -53,4 +61,46 @@ public class CgdCommunicationLog extends CgdCommunicationLog_Base {
     public boolean isSuccess() {
         return getSuccess();
     }
+
+    @Override
+    public String getDataSent() {
+        String result = "";
+        String compressedData = super.getDataSent();
+        try {
+            result = compressedData.isBlank() ? result : uncompressData(Base64.getDecoder().decode(compressedData));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static String compressData(String dataSent) {
+        byte[] byteToEncode = new byte[0];
+        try {
+            byteToEncode = compress(dataSent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Base64.getEncoder().encodeToString(byteToEncode);
+    }
+
+    private static byte[] compress(String dataSent) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(byteArrayOutputStream);
+        gzip.write(dataSent.getBytes());
+        gzip.close();
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        byteArrayOutputStream.close();
+        return byteArray;
+    }
+
+    private String uncompressData(byte[] compressedData) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(compressedData);
+        GZIPInputStream gunzip = new GZIPInputStream(byteArrayInputStream);
+        byte[] byteArray = IOUtils.toByteArray(gunzip);
+        gunzip.close();
+        byteArrayInputStream.close();
+        return new String(byteArray);
+    }
+
 }
